124d8dc91b04e159a63aeff54abc408e39b69af9
Israel Guzman <isragp4@gmail.com>
2020-05-30 03:33:11 -0500

    Creates product-card in ui library

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ProductCardComponent],
+  exports: [
+    ProductCardComponent
+  ]
 })
 export class UiModule {}
